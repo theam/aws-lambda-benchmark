010aecaeac1c4c378fe9a98f8898817891f88ba8
Tai Nguyen Bui <tai@theagilemonkeys.com>
2019-07-17 13:25:56 +0100

    adds another test with c# that uses a layer

---
+++ examples/rest-api/csharp-lambda-layer/functions/UpdateProduct.cs
@@ -0,0 +1,52 @@
+﻿using Amazon.Lambda.Core;
+using System;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Threading.Tasks;
+using RestApiServices;
+using Newtonsoft.Json;
+using Models;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace RestApi
+{
+    public class UpdateProduct
+    {
+        ProductService service = new ProductService(Environment.GetEnvironmentVariable("PRODUCTS_TABLE_NAME"));
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public async Task<APIGatewayProxyResponse> Handler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var body = request.Body;
+            Product product = JsonConvert.DeserializeObject<Product>(body);
+
+            if (string.IsNullOrEmpty(product.sku))
+            {
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 400,
+                    Body = "sku cannot be empty"
+                };
+            }
+
+            try
+            {
+                Document item = await service.UpdateItem(product);
+
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 200,
+                    Body = item == null ? "" : item.ToJson()
+                };
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e);
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 500,
+                    Body = ""
+                };
+            }
+        }
+    }
+}
