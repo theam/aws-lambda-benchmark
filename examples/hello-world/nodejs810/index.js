84becaa552fbee310fba6209b4c479466f9c58f3
Alexandre Bolzon <blzn@mail.ru>
2019-09-12 16:41:54 -0300

    fixing readme table + compacting nodejs code

--- examples/hello-world/nodejs810/index.js
+++ examples/hello-world/nodejs810/index.js
@@ -1,10 +1,5 @@
-exports.handler = async (event, context, callback) => {
-
-    var response = {
-        statusCode: 200,
-        body: JSON.stringify("hello"),
-        isBase64Encoded: false
-    };
-    
-    callback(null, response)
-};
+exports.handler = async () => ({
+  statusCode: 200,
+  body: `"hello"`,
+  isBase64Encoded: false
+});
